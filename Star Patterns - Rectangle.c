c7c4eab9d04db96c928e50d2344cd6471307e52b
pabasaraRathnayake <66038907+pabasaraRatnayake@users.noreply.github.com>
2020-08-14 20:27:18 +0530

    Add files via upload

---
+++ Star Patterns - Rectangle.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+    int columns, rows;
+
+    printf("Enter the number of rows : ");
+    scanf("%d",&rows);
+    printf("Enter the number of columns : ");
+    scanf("%d",&columns);
+
+    for(int i=1; i<=rows; i++){
+        for(int j=1; j<=columns; j++){
+            printf("*   ");
+        }
+        printf("\n");
+    }
+
+
+    return 0;
+}
