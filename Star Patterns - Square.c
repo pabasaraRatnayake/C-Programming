60eff189d84520c0daf7da6165c572264afeb1c4
pabasaraRathnayake <66038907+pabasaraRatnayake@users.noreply.github.com>
2020-08-14 20:26:23 +0530

    Add files via upload

---
+++ Star Patterns - Square.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int length;
+
+    printf("Enter one side length of the square : ");
+    scanf("%d",&length);
+
+    for(int i=1; i<=length; i++){
+        for(int j=1; j<=length; j++){
+            printf("*   ");
+        }
+        printf("\n");
+    }
+
+
+    return 0;
+}
